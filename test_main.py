41c62ccd402ce943819bc178b6806fad98e2c66c
RESUL <resu1.pv@gmail.com>
2026-07-25 11:16:26 +0300

    add pytest tests it is test_del_tasks and test_wrong_password

--- test_main.py
+++ test_main.py
@@ -30,24 +30,20 @@ def test_add_tasks():
     assert test_add_task.status_code == 200
 
 def test_del_tasks():
-    login_response = client.post(
-        "/login",
-        json={"email": "vova@gmail.com", "password": "12345678", "username": "vova"},
-    )
-    token = login_response.json()["token"]
-    headers = {"Authorization": f"Bearer {token}"}
-
-    add_task_response = client.post(
-        "/add_tasks",
-        json={"task": "Task_del", "deadline": "2024-06-30T12:00:00"},
-        headers=headers,
-    )
-    assert add_task_response.status_code == 200
-
-    tasks_response = client.get("/read_tasks_deadline", headers=headers)
-    tasks = tasks_response.json()
+    login_response = client.post('/login', json={'username': 'vova', 'email': 'vova@gmail.com', 'password': '12345678'})
+    token = login_response.json()['token']
+    headers = {'Authorization': f'Bearer {token}'}
+    test_add_task = client.post('add_tasks', json={"task": "Task_add", "deadline": "2024-06-30T12:00:00"}, headers=headers)
+    assert test_add_task.status_code == 200
+    
+    test_read_task = client.get('/read_tasks_deadline', headers=headers)
+    tasks = test_read_task.json()
     assert tasks
 
-    task_id = tasks[-1]["id"]
-    test_del_task = client.delete(f"/del_tasks/{task_id}", headers=headers)
+    task_id = tasks[-1]['id']
+    test_del_task = client.delete(f'/del_tasks/{task_id}', headers=headers)
     assert test_del_task.status_code == 200
+
+def test_wrong_password():
+    wrong_response = client.post('/login', json={'username': 'vova', 'email': 'vova@gmail.com', 'password': '123456789'})
+    assert wrong_response.status_code == 401
