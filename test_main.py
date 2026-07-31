9910e09ea9fb0aee9c4cdbd5c18ac8eea8905c00
RESUL <resu1.pv@gmail.com>
2026-07-31 14:46:00 +0300

    Added test_register_user and test_register_existing_user

--- test_main.py
+++ test_main.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 def test_read_tasks():
     login_response = client.post(
         "/login",
-        json={"email": "vova@gmail.com", "password": "12345678", "username": "vova"},
+        json={"email": "resul@gmail.com", "password": "12345678", "username": "Resul"},
     )
     token = login_response.json()["token"]
     headers = {"Authorization": f"Bearer {token}"}
@@ -18,7 +18,7 @@ def test_read_tasks():
 def test_add_tasks():
     login_response = client.post(
         "/login",
-        json={"email": "vova@gmail.com", "password": "12345678", "username": "vova"},
+        json={"email": "resul@gmail.com", "password": "12345678", "username": "Resul"},
     )
     token = login_response.json()["token"]
     headers = {"Authorization": f"Bearer {token}"}
@@ -30,7 +30,7 @@ def test_add_tasks():
     assert test_add_task.status_code == 200
 
 def test_del_tasks():
-    login_response = client.post('/login', json={'username': 'vova', 'email': 'vova@gmail.com', 'password': '12345678'})
+    login_response = client.post('/login', json={'username': 'resul', 'email': 'resul@gmail.com', 'password': '12345678'})
     token = login_response.json()['token']
     headers = {'Authorization': f'Bearer {token}'}
     test_add_task = client.post('add_tasks', json={"task": "Task_add", "deadline": "2024-06-30T12:00:00"}, headers=headers)
@@ -45,5 +45,13 @@ def test_del_tasks():
     assert test_del_task.status_code == 200
 
 def test_wrong_password():
-    wrong_response = client.post('/login', json={'username': 'vova', 'email': 'vova@gmail.com', 'password': '123456789'})
+    wrong_response = client.post('/login', json={'username': 'resul', 'email': 'resul@gmail.com', 'password': '123456789'})
     assert wrong_response.status_code == 401
+
+def test_register_user():
+    register_response = client.post('/register', json={'username': 'test_user', 'email': 'test@gmail.com', 'password': '12345678'})
+    assert register_response.status_code == 200
+
+def test_register_existing_user():
+    register_response = client.post('/register', json={'username': 'test_user', 'email': 'test@gmail.com', 'password': '12345678'})
+    assert register_response.status_code == 409
